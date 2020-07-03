2632dd5cc01b5bdcc8c344254ec55a64ffd16fa3
Matt Dray <mwdray@gmail.com>
2020-07-03 09:29:26 +0100

    Annotate ghdump hex code

--- r/ghdump_hex.R
+++ r/ghdump_hex.R
@@ -2,19 +2,37 @@
 # Created originally 14 June 2020
 # Matt Dray
 
-library(magick)
-library(bunny)
-library(ggforce)
+# See https://www.ddrive.no/post/making-hex-and-twittercard-with-bunny-and-magick/
+# Font installed via https://google-webfonts-helper.herokuapp.com/fonts
 
+# Attach packages 
+# Note that {ggforce} should be installed
+library(magick)
+library(bunny)  # remotes::install_github("dmi3kno/bunny")
+
+# Read in image as magick-image class
 img <- image_read("img/landfill2.png")
 
-ghdump_hex <- image_canvas_hex(border_color="#000000", border_size = 15) %>% 
-  bunny::image_compose(img, gravity = "west", offset = "+120+270") %>% 
-  image_annotate(
-    "{ghdump}",
-    font = "Open Sans Condensed", color = "#000000",
-    gravity = "center", location = "+230-400",
-    size = 250, degrees = 30
+# Create hex
+ghdump_hex <- 
+  image_canvas_hex(  # generate basic hex
+    border_color = "#000000",
+    border_size = 15
+  ) %>% 
+  image_compose(  # add image
+    image = img,  # our magick-image-class object
+    gravity = "west",  # 'non-absolute' positioning
+    offset = "+120+270"  # shift relative to gravity
+  ) %>% 
+  image_annotate(  # add text
+    text = "{ghdump}",
+    font = "Open Sans Condensed",  # installed to my machine
+    color = "#000000",
+    gravity = "center",  
+    location = "+230-400",
+    size = 250,   # font-size (pixels)
+    degrees = 30  # rotation angle
   )
-  
+
+# Write the image to disk
 image_write(ghdump_hex, here::here("output", "ghdump_hex.png"))
