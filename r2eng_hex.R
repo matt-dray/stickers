1c7f8102967435b153667a8eaec4f15ebf8963fb
Matt Dray <mwdray@gmail.com>
2020-08-11 22:56:51 +0100

    Add alternate URL placement on r2eng hex

--- r2eng_hex.R
+++ r2eng_hex.R
@@ -24,7 +24,7 @@ image_canvas_hex(
     size = 450,
     color = "#00FFFF",
     gravity = "center",
-    location = "+0-160"
+    location = "+0-180"
   ) %>% 
   image_annotate(
     # "matt-dray.github.io/r2eng",
@@ -33,15 +33,33 @@ image_canvas_hex(
     size = 58,
     color = "#00FFFF",
     gravity = "center",
-    location = "+0+250"
+    location = "+0+200"
   )
 
-# image_annotate(
-#   "m a t t - d r a y . g i t h u b . i o / r 2 e n g",
-#   font = my_font,
-#   size = 58,
-#   color = "#000000",
-#   gravity = "center",
-#   location = "+370+650",
-#   degrees = 330
-# )
+image_canvas_hex(
+  fill_color = "#00FFFF",
+  border_color = "#000000",
+  border_size = 5
+) %>% 
+  image_compose(
+    box_filled,
+    gravity = "center",
+    offset = "+0+50"
+  ) %>% 
+  image_annotate(
+    text = "r2eng",
+    font = my_font,
+    size = 450,
+    color = "#00FFFF",
+    gravity = "center",
+    location = "+0-100"
+  ) %>% 
+image_annotate(
+  "m a t t - d r a y . g i t h u b . i o / r 2 e n g",
+  font = my_font,
+  size = 58,
+  color = "#000000",
+  gravity = "center",
+  location = "+370+650",
+  degrees = 330
+)
