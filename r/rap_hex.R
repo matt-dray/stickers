18ba83128dfc44c7692cdb50e6eea1947da83cea
Matt Dray <18232097+matt-dray@users.noreply.github.com>
2023-05-23 11:42:38 +0100

    Remove old URL from RAP sticker

--- r/rap_hex.R
+++ r/rap_hex.R
@@ -11,11 +11,11 @@ sticker(
   # package name options
   package = "RAP",  # package name
   p_color = "black",  # font colour
-  p_size = 9,  # font size
+  p_size = 30,  # font size
   p_y = 1.3,  # y position for package name
   
   # subplot options
-  subplot = "https://upload.wikimedia.org/wikipedia/commons/9/9e/Warp_pipe_edited.PNG",
+  subplot = "img/warp-pipe.png",  # https://upload.wikimedia.org/wikipedia/commons/9/9e/Warp_pipe_edited.PNG
   s_x = 1,
   s_y = 1,
   s_width = 0.6,
@@ -24,10 +24,10 @@ sticker(
   h_fill = "white",  # hexagon fill (area to side of pipe isn't transparent)
   h_color = "#159c03", # border (same dark green as pipe from url)
 
-  # url options
-  url = "ukgovdatascience.github.io/rap-website",
-  u_size = 1.33,
-  u_family = "sans",
+  # # url options
+  # url = "ukgovdatascience.github.io/rap-website",
+  # u_size = 1.33,
+  # u_family = "sans",
   
   # save options
   filename = "output/rap_hex.png"
